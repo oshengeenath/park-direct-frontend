41739dfa827ee1c263cc023cbda6383247941cc8
Thathsarani-Sandarekha <thathsaranisandarekha@gmail.com>
2024-04-14 01:22:57 +0530

    Fixed confirmed bookings screen

--- lib/views/officer_dashboard/confirmed_requests_screen.dart
+++ lib/views/officer_dashboard/confirmed_requests_screen.dart
@@ -1,7 +1,36 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-class ConfirmedRequestsScreen extends StatelessWidget {
+import '../../models/booking_model.dart';
+import '../../util/app_constants.dart';
+
+class ConfirmedRequestsScreen extends StatefulWidget {
   const ConfirmedRequestsScreen({super.key});
+
+  @override
+  State<ConfirmedRequestsScreen> createState() => _ConfirmedRequestsScreenState();
+}
+
+class _ConfirmedRequestsScreenState extends State<ConfirmedRequestsScreen> {
+  late Future<List<Booking>> pendingRequests;
+
+  @override
+  void initState() {
+    super.initState();
+    pendingRequests = fetchConfirmedRequests();
+  }
+
+  Future<List<Booking>> fetchConfirmedRequests() async {
+    final response = await http.get(Uri.parse(AppConstants.baseUrl + AppConstants.officerFetchAllConfirmedBookings));
+
+    if (response.statusCode == 200) {
+      List jsonResponse = json.decode(response.body);
+      return jsonResponse.map((data) => Booking.fromJson(data)).toList();
+    } else {
+      throw Exception('Failed to load confirmed bookings');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,6 +44,41 @@ class ConfirmedRequestsScreen extends StatelessWidget {
             fontWeight: FontWeight.bold,
           ),
         ),
+      ),
+      body: FutureBuilder<List<Booking>>(
+        future: pendingRequests,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else {
+            return Scrollbar(
+              thumbVisibility: true,
+              child: ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  final booking = snapshot.data![index];
+                  return Container(
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    decoration: const BoxDecoration(
+                      color: Color.fromARGB(255, 244, 240, 240),
+                      border: Border(
+                        bottom: BorderSide(
+                          color: Color(0xFFE3E2E2), // Color for the bottom line
+                          width: 1.0, // Thickness of the bottom line
+                        ),
+                      ),
+                    ),
+                    child: ListTile(
+                      title: Text("The vehicle '${booking.vehicleNumber}' is requesting a location."),
+                    ),
+                  );
+                },
+              ),
+            );
+          }
+        },
       ),
     );
   }
