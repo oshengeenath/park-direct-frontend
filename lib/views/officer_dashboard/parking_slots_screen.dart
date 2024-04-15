87ad81aa8203967fac5d0dd6cb4c631210a3f773
nisuli <134255746+nisuli@users.noreply.github.com>
2024-04-15 11:19:09 +0530

    Implemented the parking slots screen

--- lib/views/officer_dashboard/parking_slots_screen.dart
+++ lib/views/officer_dashboard/parking_slots_screen.dart
@@ -1,10 +1,70 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: library_private_types_in_public_api, use_build_context_synchronously
 
-class ParkingSlotsScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:developer' as developer;
+
+import '../../models/parking_slot_model.dart';
+import '/util/app_constants.dart';
+
+class ParkingSlotsScreen extends StatefulWidget {
   const ParkingSlotsScreen({super.key});
 
   @override
+  _ParkingSlotsScreenState createState() => _ParkingSlotsScreenState();
+}
+
+class _ParkingSlotsScreenState extends State<ParkingSlotsScreen> {
+  List<ParkingSlot> parkingSlots = [];
+  bool isLoading = true;
+  String? selectedSlotId;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchParkingSlots();
+  }
+
+  Future<void> fetchParkingSlots() async {
+    const url = '${AppConstants.baseUrl}/officer/all-parking-slots';
+    try {
+      final response = await http.get(Uri.parse(url));
+      developer.log('HTTP Response status: ${response.statusCode}');
+      final List<dynamic> fetchedSlots = json.decode(response.body);
+
+      if (fetchedSlots.isEmpty) {
+        developer.log('No slots were fetched.');
+      }
+
+      final List<ParkingSlot> loadedSlots = [];
+      for (var slot in fetchedSlots) {
+        loadedSlots.add(ParkingSlot.fromJson(slot));
+        developer.log('Loaded slot: ${slot['slotId']}');
+      }
+
+      loadedSlots.sort((a, b) => a.slotId.compareTo(b.slotId));
+
+      setState(() {
+        parkingSlots = loadedSlots;
+        isLoading = false;
+      });
+    } catch (error) {
+      developer.log('Error fetching slots: $error');
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
+    if (isLoading) {
+      return const Scaffold(
+        body: Center(child: CircularProgressIndicator()),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -15,6 +75,37 @@ class ParkingSlotsScreen extends StatelessWidget {
             fontWeight: FontWeight.bold,
           ),
         ),
+      ),
+      body: GridView.builder(
+        padding: const EdgeInsets.all(10),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 5,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+          childAspectRatio: 1,
+        ),
+        itemCount: parkingSlots.length,
+        itemBuilder: (context, index) {
+          final parkingSlot = parkingSlots[index];
+          bool isSelected = selectedSlotId == parkingSlot.slotId && !parkingSlot.isBooked;
+
+          return Container(
+            decoration: BoxDecoration(
+              color: isSelected
+                  ? const Color(0xFFFFC700)
+                  : parkingSlot.isBooked
+                      ? Colors.red
+                      : const Color(0xFFF3F6FF),
+              border: Border.all(color: Colors.black),
+            ),
+            child: Center(
+              child: Text(
+                parkingSlot.slotId,
+                style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
