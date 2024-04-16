a36ccae6d7166b788c3943cbabe06c9b6d748fcb
osheng7 <oshengeenath2002@gmail.com>
2024-04-16 21:38:20 +0530

    Updated the baseURL

--- lib/util/app_constants.dart
+++ lib/util/app_constants.dart
@@ -1,5 +1,5 @@
 class AppConstants {
-  static const String baseUrl = "http://10.0.2.2:8000";
+  static const String baseUrl = "http://park-direct-backend.northeurope.cloudapp.azure.com:3004";
 
   static const String sendVerificationEmail = "/auth/send-verification-email";
   static const String verifyEmail = "/auth/verify-email";
@@ -7,10 +7,8 @@ class AppConstants {
   static const String loginUser = "/auth/login-user";
   static const String forgotPassword = "/auth/forgot-password";
   static const String resetPassword = "/auth/reset-password";
-
   static const String vehicleOwnerBookSlot = "/vehicleOwner/book-slot";
   static const String vehicleOwnerGetAllBookings = "/vehicleOwner/get-all-bookings/chanakanbonline@gmail.com";
-
   static const String officerFetchAllPendingBookings = "/officer/fetch-all-pending-requests";
   static const String officerFetchAllConfirmedBookings = "/officer/fetch-all-confirmed-bookings";
   static const String officerConfirmBookingRequest = "/officer/confirm-booking-request";
